17e79361451479ab25e06954278d497f74ec1036
Per Wahlström <per.wahlstrom@gmail.com>
2024-08-17 09:04:18 +0200

    Firefox version 129.0.1

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download firefox into files directory if it's not there already
-export version="126.0.1"
+export version="129.0.1"
 export filename="firefox-$version.tar.bz2"
 export url="https://download-installer.cdn.mozilla.net/pub/firefox/releases/$version/linux-x86_64/en-US/$filename"
 mkdir -p downloads
